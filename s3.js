ccdeed091250881671a48abf6163320df67720fb
Bread Loaf <177406967+BreadLoaf0509@users.noreply.github.com>
2025-04-19 08:19:55 +0800

    complete extract template
    This will server as the fail safe when the event of the system not working happens

--- s3.js
+++ s3.js
@@ -7,9 +7,19 @@ const accessKeyId = process.env.AWS_ACCESS_KEY;
 const secretAccessKey = process.env.AWS_SECRET_KEY;
 
 const s3 = new S3({
+    bucketName,
     region,
     accessKeyId,
     secretAccessKey
 });
 
-module.exports = {s3};
+function getImageStream(imageKey){
+    return s3.getObject({Bucket: bucketName, Key: imageKey}).createReadStream();
+}
+
+function deleteImage(imageKey){
+    return s3.deleteObject({Bucket: bucketName, Key: imageKey}).promise();
+}
+
+
+module.exports = {s3, getImageStream, deleteImage};
--- server.js
+++ server.js
@@ -1,28 +1,35 @@
+var user_id = 1; // This is a placeholder. You can replace it with the actual user ID from your authentication system.
+
+// changes  
+
 const express = require("express");
 const multer = require("multer");
+const path = require("path");
 
-// const path = require('path');
 // const fs = require('fs');
 // const util = require('util');
 // const unlinkFile = util.promisify(fs.unlink);
 
-// const multerS3 = require("multer-s3-v2");
-const {s3} = require("./s3.js");
-const {dbConnection} = require("./db.js");
+const multerS3 = require("multer-s3-v2");
+const { s3, getImageStream, deleteImage } = require("./s3.js");
+const { dbConnection } = require("./db.js");
+require("dotenv").config();
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./public/uploads");
+const storage = multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET_NAME,
+    metadata: function (req, file, cb) {
+        cb(null, { originalname: file.originalname });
     },
-    filename: function (req, file, cb) {
+    key: function (req, file, cb) {
         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
         cb(null, uniqueSuffix + path.extname(file.originalname));
-    },
-});
+    }
+})
 
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 5000000 }, // 5MB limit
+    limits: { fileSize: 10000000 }, // 5MB limit
     fileFilter: function (req, file, cb) {
         checkFileType(file, cb);
     },
@@ -52,23 +59,16 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    // new
-    // let images = [];
-    // fs.readdir('./public/uploads/', (err, files) => {
-    //     if(!err){
-    //         files.forEach(file => {
-    //             images.push(file);
-    //         });
-    res.render("index", { images: images });
-    //     } else{
-    //         console.error(err);
-    //     }
-    // })
+    dbConnection.query("SELECT * FROM images WHERE user_id = ?", [user_id], (err, result) => {
+        if(!err) res.render("index", { images: result });
+        else throw new Error(err)
+    })
 });
 
 app.post("/upload", (req, res) => {
     upload(req, res, (err) => {
         if (!err && req.files != "") {
+            saveImagesInDB(req.files)
             res.status(200).send();
         } else if (!err && req.files == "") {
             res.statusMessage = "Please select an image to upload!";
@@ -90,14 +90,46 @@ app.put("/delete", (req, res) => {
         // deleteImages.forEach((image) => {
         //     unlinkFile(`./public/uploads/` + image);
         // });
+        deleteImagesFromS3(deleteImages);
+        deleteImagesFromDB(deleteImages);
         res.statusMessage = "Image deleted successfully!";
         res.status(200).end();
     }
 });
 
+app.get("/:image_key", (req, res) => {
+    const readStream = getImageStream(req.params.image_key);
+    readStream.pipe(res);
+})
+
+function saveImagesInDB(images) {
+    for (let i = 0; i < images.length; i++) {
+        dbConnection.query("INSERT INTO images (user_id, image_key) VALUES (?, ?)",
+            [user_id, images[i].key], (err, result) => {
+                if (err) throw new Error(err);
+            })
+    }
+
+}
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+function deleteImagesFromS3(images){
+    for(let i = 0; i < images.length; i++){
+        deleteImage(images[i]);
+    }
+}
+
+function deleteImagesFromDB(images){
+    for(let i = 0; i < images.length; i++){
+        dbConnection.query("DELETE FROM images WHERE user_id = ? AND image_key = ?",
+        [user_id, images[i]], (err, result) => {
+            if (err) throw new Error(err);
+        } )
+    }
+}
 
 // changes
 
